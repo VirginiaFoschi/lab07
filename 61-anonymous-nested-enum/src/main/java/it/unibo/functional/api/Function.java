9bc6aa9820c6796b9832448524328ac33a36ee1e
Virginia Foschi <virginia.foschi@studio.unibo.it>
2022-11-07 11:40:45 +0100

    file Function.java modified

--- 61-anonymous-nested-enum/src/main/java/it/unibo/functional/api/Function.java
+++ 61-anonymous-nested-enum/src/main/java/it/unibo/functional/api/Function.java
@@ -7,7 +7,7 @@ package it.unibo.functional.api;
  * @param <O> the output type
  */
 public interface Function<I, O> {
-
+    
     /**
      * Calls the function.
      *
@@ -23,7 +23,15 @@ public interface Function<I, O> {
      * @param <T> the input (and output) type of the function
      */
     static <T> Function<T, T> identity() {
-        return null;
+        return new Function<T,T> () {
+
+            public T call(T input) {
+                // TODO Auto-generated method stub
+                return input;
+            }
+
+        };
     }
 
 }
+
