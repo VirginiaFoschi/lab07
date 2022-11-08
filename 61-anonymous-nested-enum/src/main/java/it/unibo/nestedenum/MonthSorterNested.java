34b3114a58f5f07beaea541dd0dfd4c4a52ad8bb
Virginia Foschi <virginia.foschi@studio.unibo.it>
2022-11-08 10:22:44 +0100

    implemented enum Month

--- 61-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 61-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -18,4 +18,35 @@ public final class MonthSorterNested implements MonthSorter {
     public Comparator<String> sortByOrder() {
         return null;
     }
+
+    private enum Month{
+        GENNAIO(31,"Gennaio"),
+        FEBBRAIO(28,"Febbraio"),
+        MARZO(31,"Marzo"),
+        APRILE(30,"Aprile"),
+        MAGGIO(31,"Maggio"),
+        GIUGNO(30,"Giugno"),
+        LUGLIO(31,"Luglio"),
+        AGOSTO(31,"Agosto"),
+        SETTEMBRE(30,"Settembre"),
+        OTTOBRE(31,"Ottobre"),
+        NOVEMBRE(30,"Novembre"),
+        DICEMBRE(31,"Dicembre");
+
+        private int days;
+        private String actualName;
+
+        private Month(int days, String actualName) {
+            this.days = days;
+            this.actualName=actualName;
+        }
+        
+        Month fromString(String name) {
+            for (Month mese : Month.values()) {
+                if(mese.actualName.equals(name)) return mese;
+            }
+            return null;
+        }
+
+    }
 }
